1d46c31af5aee4624ab60c79e2d3db8823797f91
SongYibao <songybmail@foxmail.com>
2024-04-23 21:22:31 +0800

    修改界面默认布局

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -8,14 +8,12 @@ const Settings: ProLayoutProps & {
   logo?: string;
 } = {
   navTheme: 'light',
-  // 拂晓蓝
-  colorPrimary: '#1890ff',
-  layout: 'mix',
+  layout: 'side',
   contentWidth: 'Fluid',
   fixedHeader: false,
-  fixSiderbar: true,
-  colorWeak: false,
-  title: 'Ant Design Pro',
+  colorPrimary: '#1677FF',
+  splitMenus: false,
+  title: 'yb-api',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
