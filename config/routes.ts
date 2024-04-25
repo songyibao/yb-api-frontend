b686ea30e158b692940131de4499edbb75c1f5ac
SongYibao <songybmail@foxmail.com>
2024-04-25 20:54:44 +0800

    完善路由配置

--- config/routes.ts
+++ config/routes.ts
@@ -1,9 +1,21 @@
 export default [
+  { name: '主页', path: '/', component: './Index', icon: 'smile' },
+  {
+    name: '接口详细信息',
+    path: '/interface_info/:id',
+    component: './InterfaceInfo',
+    icon: 'smile',
+    hideInMenu: true,
+  },
   {
     path: '/user',
     layout: false,
-    routes: [{ name: '登录', path: '/user/login', component: './User/Login' }],
+    routes: [
+      { name: '登录', path: '/user/login', component: './User/Login' },
+      { name: '注册', path: '/user/register', component: './User/Register' },
+    ],
   },
+
   // { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
@@ -11,7 +23,16 @@ export default [
     icon: 'crown',
     access: 'canAdmin',
     routes: [
-      { name: '接口管理', icon: 'table', path: '/admin/interface', component: './InterfaceInfo' },
+      {
+        path: '/admin',
+        redirect: '/admin/interface',
+      },
+      {
+        name: '接口管理',
+        icon: 'table',
+        path: '/admin/interface',
+        component: './Admin/InterfaceInfo',
+      },
     ],
   },
   { path: '*', layout: false, component: './404' },
